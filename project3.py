6ba13ff4c6e0c42a249d01d3e3896f0d5771aaa4
Anikesh Jhadi <74179827+AnikeshJhadi@users.noreply.github.com>
2021-08-31 20:02:00 +0530

    Project 3

---
+++ project3.py
@@ -0,0 +1,10 @@
+set1 ={1,2,3,4,5}
+set2 ={1,3,5,7}
+z=set1.union(set2)
+print("Union of Set 1 and Set 2:-"+str(z))
+z=set1.intersection(set2)
+print("Intersection of Set 1 and Set 2:-"+str(z))
+z=set1.difference(set2)
+print("Difference of Set 1 and Set 2:-"+str(z))
+z=set1.symmetric_difference(set2)
+print("Symmetric Difference of Set 1 and Set 2:-"+str(z))
